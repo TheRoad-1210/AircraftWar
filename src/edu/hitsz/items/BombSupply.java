575228645b436c24812c1a4f8a02c3e88120bef9
deequoique <puppy_road@163.com>
2022-03-17 20:37:19 +0800

    炸弹（todo

---
+++ src/edu/hitsz/items/BombSupply.java
@@ -0,0 +1,13 @@
+package edu.hitsz.items;
+
+public class BombSupply extends AbstractProp{
+
+    public BombSupply(int locationX, int locationY) {
+        super(locationX, locationY);
+    }
+
+    @Override
+    public int form() {
+        return 0;
+    }
+}
